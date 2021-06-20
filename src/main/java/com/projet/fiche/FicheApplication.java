9a05cba3401a7e8f86de0e26a1dfddfe0ae3571e
Oussama <ous-honor@live.fr>
2021-06-20 23:31:18 +0200

    Initialisation

---
+++ src/main/java/com/projet/fiche/FicheApplication.java
@@ -0,0 +1,13 @@
+package com.projet.fiche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FicheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FicheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projet/fiche/FicheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.fiche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FicheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
